c146d016d23f92789afff5ee50d1cd0e3fea7353
Jakub Urban <urban@ipp.cas.cz>
2014-08-01 17:08:27 +0200

    added insertion from https://gist.github.com/jaredks/6276032

--- pydons.py
+++ pydons.py
@@ -53,6 +53,22 @@ class StrONGDict(_OrderedDict):
         else:
             raise AttributeError("no attribute '{}'".format(item))
 
+    # insertion inspired by https://gist.github.com/jaredks/6276032
+    def __insertion(self, link_prev, key_value):
+        key, value = key_value
+        if link_prev[2] != key:
+            if key in self:
+                del self[key]
+            link_next = link_prev[1]
+            self._OrderedDict__map[key] = link_prev[1] = link_next[0] = [link_prev, link_next, key]
+        dict.__setitem__(self, key, value)
+
+    def insert_after(self, existing_key, key_value):
+        self.__insertion(self._OrderedDict__map[existing_key], key_value)
+
+    def insert_before(self, existing_key, key_value):
+        self.__insertion(self._OrderedDict__map[existing_key][0], key_value)
+
     def __dir__(self):
         d = []
         d += self.keys()
