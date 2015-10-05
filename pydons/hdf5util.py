90209bab90e8485dafde0c7d4c021e1cf9a0e197
Jakub Urban <urban@ipp.cas.cz>
2015-10-05 10:46:09 +0200

    updated hdf5storage marshaller from upstream, py3 compatible

--- pydons/hdf5util.py
+++ pydons/hdf5util.py
@@ -1,6 +1,16 @@
 from hdf5storage.Marshallers import TypeMarshaller
 # import pydons
 import h5py
+import six
+import distutils
+
+# Ubuntu 12.04's h5py doesn't have __version__ set so we need to try to
+# grab the version and if it isn't available, just assume it is 2.0.
+try:
+    _H5PY_VERSION = h5py.__version__
+except:
+    _H5PY_VERSION = '2.0'
+
 
 from hdf5storage.utilities import *
 # from hdf5storage import lowlevel
@@ -13,13 +23,43 @@ class MatStructMarshaller(TypeMarshaller):
         self.python_attributes |= set(['Python.Fields'])
         self.matlab_attributes |= set(['MATLAB_class'])
         self.types = [MatStructType, dict]
-        self.python_type_strings = ['%s.%s' % (MatStructType.__module__, MatStructType.__name__)]
+        self.python_type_strings = ['{}.{}'.format(MatStructType.__module__, MatStructType.__name__),
+                                    'dict']
         self.__MATLAB_classes = {MatStructType: 'struct'}
         # Set matlab_classes to empty since NumpyScalarArrayMarshaller
         # handles Groups by default now.
         self.matlab_classes = list(self.__MATLAB_classes.values())
 
     def write(self, f, grp, name, data, type_string, options):
+        # Check for any field names that are not unicode since they
+        # cannot be handled. Also check for null characters and /
+        # characters since they can't be handled either. How it is
+        # checked (what type it is) and the error message are different
+        # for each Python version.
+
+        if sys.hexversion >= 0x03000000:
+            for fieldname in data:
+                if not isinstance(fieldname, str):
+                    raise NotImplementedError('Dictionaries with non-'
+                                              + 'str keys are not '
+                                              + 'supported: '
+                                              + repr(fieldname))
+                if '\x00' in fieldname or '/' in fieldname:
+                    raise NotImplementedError('Dictionary keys with ' \
+                        + "null characters ('\x00') and '/' are not " \
+                        + 'supported.')
+        else:
+            for fieldname in data:
+                if not isinstance(fieldname, unicode):
+                    raise NotImplementedError('Dictionaries with non-'
+                                              + 'unicode keys are not '
+                                              + 'supported: '
+                                              + repr(fieldname))
+                if u'\x00' in fieldname or u'/' in fieldname:
+                    raise NotImplementedError('Dictionary keys with ' \
+                        + "null characters ('\x00') and '/' are not " \
+                        + 'supported.')
+
         # If the group doesn't exist, it needs to be created. If it
         # already exists but is not a group, it needs to be deleted
         # before being created.
@@ -48,7 +88,7 @@ class MatStructMarshaller(TypeMarshaller):
         # doing MATLAB compatibility (otherwise, the attribute needs to
         # be deleted).
         for k, v in data.items():
-            k = unicode(k)
+            k = six.text_type(k)
             write_data(f, grp2, k, v, None, options)
             if k in grp2:
                 if options.matlab_compatible:
@@ -62,12 +102,35 @@ class MatStructMarshaller(TypeMarshaller):
         TypeMarshaller.write_metadata(self, f, grp, name, data,
                                       type_string, options)
 
+        # Grab all the keys and sort the list.
+        fields = list(data.keys())
+
         # If we are storing python metadata, we need to set the
-        # 'Python.Fields' Attribute to be all the keys. They will be
+        # 'Python.Fields' Attribute to be all the keys.
         if options.store_python_metadata:
-            fields = list(data.keys())
             set_attribute_string_array(grp[name], 'Python.Fields',
                                        fields)
+
+        # If we are making it MATLAB compatible and have h5py version
+        # >= 2.3, then we can set the MATLAB_fields Attribute as long as
+        # all keys are mappable to ASCII. Otherwise, the attribute
+        # should be deleted. It is written as a vlen='S1' array of
+        # bytes_ arrays of the individual characters.
+        if options.matlab_compatible \
+                and distutils.version.LooseVersion(_H5PY_VERSION) \
+                >= distutils.version.LooseVersion('2.3'):
+            try:
+                dt = h5py.special_dtype(vlen=np.dtype('S1'))
+                fs = np.empty(shape=(len(fields),), dtype=dt)
+                for i, s in enumerate(fields):
+                    fs[i] = np.array([c.encode('ascii') for c in s],
+                                     dtype='S1')
+            except UnicodeDecodeError:
+                del_attribute(grp[name], 'MATLAB_fields')
+            else:
+                set_attribute(grp[name], 'MATLAB_fields', fs)
+        else:
+            del_attribute(grp[name], 'MATLAB_fields')
 
         # If we are making it MATLAB compatible, the MATLAB_class
         # attribute needs to be set for the data type. If the type
@@ -75,26 +138,11 @@ class MatStructMarshaller(TypeMarshaller):
         # the attributes need to be deleted.
 
         tp = type(data)
-        if options.matlab_compatible and tp in self.types \
-                and tp in self.__MATLAB_classes:
+        if options.matlab_compatible and tp in self.__MATLAB_classes:
             set_attribute_string(grp[name], 'MATLAB_class',
                                  self.__MATLAB_classes[tp])
-            # TODO this does not work because Matlab stores field names
-            # in a weird way (H5T_VLEN(:,:) array of H5T_STRING(1))
-            # set_attribute_string_array(grp[name], 'MATLAB_fields',
-            #                            fields)
         else:
             del_attribute(grp[name], 'MATLAB_class')
-
-        # Write an array of all the fields to the attribute that lists
-        # them.
-        #
-        # NOTE: Can't make it do a variable length set of strings like
-        # MATLAB likes. However, not including them seems to cause no
-        # problem.
-        #
-        # set_attribute_string_array(grp[name], \
-        #     'MATLAB_fields', [k for k in data])
 
     def read(self, f, grp, name, options):
         # If name is not present or is not a Group, then we can't read
